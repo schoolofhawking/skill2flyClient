5cddcde02a0405285b03f116d24bdb374af32f9a
unknown <umar@cybaze.com>
2021-10-10 21:48:50 +0530

    index page pending sections completed

---
+++ src/components/user/Index/Banners/Memberships.js
@@ -0,0 +1,102 @@
+import React from "react";
+
+export default function Memberships() {
+    return (
+        <section class="package-section" style={{backgroundImage: "url(assets/images/home/package-bg.jpg)"}}>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2 class="sec-title"><span>Online Learning</span> Offers 3 Types of Membership Packages</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div id="card-slider" class="pack-slider">
+                        <div class="package-item">
+                            <div class="pack-thumb">
+                                <img src="assets/images/home/p1.jpg" alt=""/>
+                                <div class="pack-middle">
+                                    <img src="assets/images/home/gold.png" alt=""/>
+                                </div>
+                            </div>
+                            <div class="pack-details">
+                                <div class="pack-price"><span>$750</span> $450 / 2 Year(s)</div>
+                                <h3>Gold Membership</h3>
+                                <div class="pack-meta">
+                                    <span><i class="icon_book_alt"></i>12 Lessons</span>
+                                    <span><i class="icon_toolbox_alt"></i>3 Classes</span>
+                                </div>
+                                <p>
+                                    He nicked it I don't want no agro what a load of rubbish bleeding skive off tickety-boo hunky dory chancer.
+                                </p>
+                                <a href="#" class="bisylms-btn">Apply Now</a>
+                            </div>
+                        </div>
+                        <div class="package-item">
+                            <div class="pack-thumb">
+                                <img src="assets/images/home/p2.jpg" alt=""/>
+                                <div class="pack-middle">
+                                    <img src="assets/images/home/silver.png" alt=""/>
+                                </div>
+                            </div>
+                            <div class="pack-details">
+                                <div class="pack-price"><span>$750</span> $450 / 2 Year(s)</div>
+                                <h3>Silver Membership</h3>
+                                <div class="pack-meta">
+                                    <span><i class="icon_book_alt"></i>12 Lessons</span>
+                                    <span><i class="icon_toolbox_alt"></i>3 Classes</span>
+                                </div>
+                                <p>
+                                    He nicked it I don't want no agro what a load of rubbish bleeding skive off tickety-boo hunky dory chancer.
+                                </p>
+                                <a href="#" class="bisylms-btn">Apply Now</a>
+                            </div>
+                        </div>
+                        <div class="package-item">
+                            <div class="pack-thumb">
+                                <img src="assets/images/home/p3.jpg" alt=""/>
+                                <div class="pack-middle">
+                                    <img src="assets/images/home/bronze.png" alt=""/>
+                                </div>
+                            </div>
+                            <div class="pack-details">
+                                <div class="pack-price"><span>$750</span> $450 / 2 Year(s)</div>
+                                <h3>Bronze Membership</h3>
+                                <div class="pack-meta">
+                                    <span><i class="icon_book_alt"></i>12 Lessons</span>
+                                    <span><i class="icon_toolbox_alt"></i>3 Classes</span>
+                                </div>
+                                <p>
+                                    He nicked it I don't want no agro what a load of rubbish bleeding skive off tickety-boo hunky dory chancer.
+                                </p>
+                                <a href="#" class="bisylms-btn">Apply Now</a>
+                            </div>
+                        </div>
+                        <div class="package-item">
+                            <div class="pack-thumb">
+                                <img src="assets/images/home/p2.jpg" alt=""/>
+                                <div class="pack-middle">
+                                    <img src="assets/images/home/silver.png" alt=""/>
+                                </div>
+                            </div>
+                            <div class="pack-details">
+                                <div class="pack-price"><span>$750</span> $450 / 2 Year(s)</div>
+                                <h3>Silver Membership</h3>
+                                <div class="pack-meta">
+                                    <span><i class="icon_book_alt"></i>12 Lessons</span>
+                                    <span><i class="icon_toolbox_alt"></i>3 Classes</span>
+                                </div>
+                                <p>
+                                    He nicked it I don't want no agro what a load of rubbish bleeding skive off tickety-boo hunky dory chancer.
+                                </p>
+                                <a href="#" class="bisylms-btn">Apply Now</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}

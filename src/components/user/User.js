a1917e925c992b70a8d394bda572e01c210c8e2c
Harikrishnan U A <hariua111@gmail.com>
2021-10-12 16:35:12 +0530

    Signup page ready

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Index from './Index/Index'
+import Signup from './Signup/Signup'
 
 
 function User() {
     return (
         <div>
              <Route  path="/" exact >  <Index/> </Route> 
+             <Route path="/signup"><Signup/></Route>
         </div>
     )
 }
